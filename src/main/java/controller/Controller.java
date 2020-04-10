86bc06ef8ab5ba568f81d500ae6c8c5b605800d1
ParkDooWon <50234855+ParkDooWon@users.noreply.github.com>
2020-04-10 15:25:54 +0900

    feat : Controller 생성

---
+++ src/main/java/controller/Controller.java
@@ -0,0 +1,16 @@
+package controller;
+
+import domain.menu.Menus;
+import domain.table.Tables;
+
+/**
+ *   class description
+ *
+ *   @author ParkDooWon
+ */
+public class Controller {
+	public static class run {
+		final Tables tables = new Tables();
+		final Menus menus = new Menus();
+	}
+}
